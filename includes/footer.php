7146b4427a9f95f3b12e4600ebb233e99a97f0f6
Cl-laus <lucasluisetti@gmail.com>
2025-08-05 10:38:20 +0200

    refacto du code

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+
+
+
+      <footer>
+        <p>© 2025 Find My Dream Home – Tous droits réservés.</p>
+      </footer>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -17,9 +17,9 @@
       <nav>
         <h1>Find My Dream Home</h1>
         <div id="links">
-          <div class="link">Login</div>
           <div class="link">House</div>
           <div class="link">Appartement</div>
+          <div class="link"><a href="login.php">Login</a></div>
         </div>
       </nav>
       <div id="articles">
@@ -44,9 +44,7 @@
           </div>
         </section>
       </div>
-      <footer>
-        <p>© 2025 Find My Dream Home – Tous droits réservés.</p>
-      </footer>
+    <?php require_once 'includes/footer.php'; ?>
     </main>
   </body>
 </html>
